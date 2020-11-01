1712c15eed0f02a0efe4f5f22fe18fd77b5267f6
dukdamn <dukdamn0927@gmail.com>
2020-11-01 23:56:00 +0900

    test

--- MyShoes/MyShoes/Controller/WebViewController.swift
+++ MyShoes/MyShoes/Controller/WebViewController.swift
@@ -12,16 +12,6 @@ class WebViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        guard let vc = self.presentingViewController else { return }
-                                self.dismiss(animated: true, completion: nil)
-                                if let vc3 = vc.children[vc.children.count - 3] as? WebViewController {
-                                    vc3.navigationController?.popViewController(animated: false)
-                                } else {
-                                    let vc3 = vc.children[vc.children.count - 3] as? WebViewController
-                                    print("🌝 … \(String(describing: vc3))")
-                                    print("🌝 … \(vc.children[vc.children.count - 1])")
-                                    print("🌝 … \(type(of: vc.children[1]))")
-                                }
     }
     
 
