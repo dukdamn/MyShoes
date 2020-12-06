d88c22e05852541203fd006078f3b125f7bff743
dukdamn <dukdamn0927@gmail.com>
2020-12-06 23:27:20 +0900

    readme 수정

--- MyShoes/MyShoes/APIManger/APIManager.swift
+++ MyShoes/MyShoes/APIManger/APIManager.swift
@@ -83,6 +83,8 @@ final class APIManager {
     // MARK: - GET
     
 
+    
+    
     //GET: 유저 정보
     func getUserProfile(userPK: Int? = nil, completion: @escaping (Result<User, Error>) -> Void) {
         let header: HTTPHeaders = [.authorization(bearerToken: getAccessTokenFromKeyChain())]
